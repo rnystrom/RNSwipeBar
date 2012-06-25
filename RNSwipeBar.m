52528976ac933b42a72678f2c3e9a1816e3124bb
Ryan Nystrom <rnystrom@whoisryannystrom.com>
2012-06-25 16:58:34 -0400

    Forgot to update main files

--- RNSwipeBar.m
+++ RNSwipeBar.m
@@ -1,12 +1,10 @@
+//
+//  RNSwipeBar.m
+//  RNSwipeBar
+//
 //  Created by Ryan Nystrom on 4/14/12.
+//  Copyright (c) 2012 Ryan Nystrom. All rights reserved.
 //
-//  Copyright (C) 2012 Ryan Nystrom
-//
-//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-//
-//  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-//
-//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import "RNSwipeBar.h"
 
@@ -24,6 +22,7 @@
 @synthesize parentView = _parentView;
 @synthesize delegate = _delegate;
 @synthesize barView = _barView;
+//@synthesize isHidden;
 
 #pragma mark - Init
 
@@ -63,7 +62,8 @@
         _barView = barView;
         [self addSubview:_barView];
     }
-    return self;
+//    return self;
+    return [self initWithMainView:view];
 }
 
 #pragma mark - Display methods
@@ -164,9 +164,9 @@
     [self setFrame:newFrame];
 }
 
-- (BOOL)isHidden
-{
-    return _isHidden;
-}
+//- (BOOL)isHidden
+//{
+//    return _isHidden;
+//}
 
 @end
